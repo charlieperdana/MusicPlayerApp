d94ec4f08c0914e233f1b4e4e7d7586aeafc6d41
charlieperdana <charlie.siagian@gmail.com>
2026-09-08 03:07:53 +0700

    feat: initial project commit

---
+++ MusicPlayerApp/MusicPlayerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerAppApp.swift
+//  MusicPlayerApp
+//
+//  Created by charlie siagian on 08/09/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicPlayerAppTests/MusicPlayerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerAppTests.swift
+//  MusicPlayerAppTests
+//
+//  Created by charlie siagian on 08/09/26.
+//
+
+import Testing
+@testable import MusicPlayerApp
+
+struct MusicPlayerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
